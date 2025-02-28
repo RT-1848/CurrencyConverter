e137574e5c1acaeeb5b89e59090a19a1c5c2dcc7
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-02-28 12:42:25 -0600

    Currency Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import CurrencyConverter from './components/CurrencyConverter'
 
 function App() {
 
-  return (
-    <p>Hi</p>
-  )
-}
+  return <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <CurrencyConverter />
+  </div>
+} 
 
 export default App
