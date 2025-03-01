f0f10b7264211f83e6b38146ed7eb9fbb7d7b6ec
Ryan T <150309976+RT-1848@users.noreply.github.com>
2025-03-01 15:35:50 -0600

    Removed File

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
 import CurrencyConverter from './components/CurrencyConverter'
 
 function App() {
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-gray-700 flex flex-col items-center justify-center">
       <div className="container">
         <CurrencyConverter />
       </div>
